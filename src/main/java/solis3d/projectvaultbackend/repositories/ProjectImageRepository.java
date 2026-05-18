5b98c9258c1e32c764326330fc6915f2bf1e38f1
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-05-18 17:41:19 +0200

    fixes correct project image repository

--- src/main/java/solis3d/projectvaultbackend/repositories/ProjectImageRepository.java
+++ src/main/java/solis3d/projectvaultbackend/repositories/ProjectImageRepository.java
@@ -1,17 +1,19 @@
 package solis3d.projectvaultbackend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import solis3d.projectvaultbackend.entities.ProjectImage;
+import solis3d.projectvaultbackend.entities.ProjectImageType;
 import solis3d.projectvaultbackend.entities.ProjectSoftware;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface ProjectImageRepository extends JpaRepository<ProjectSoftware, UUID> {
+public interface ProjectImageRepository extends JpaRepository<ProjectImage, UUID> {
 
     List<ProjectSoftware> findByProject_Id(UUID projectId);
 
-    boolean existsByProject_IdAndSoftware_Id(UUID projectId, UUID softwareId);
+    List<ProjectImage> findByProject_IdOrderBySortOrderAsc(UUID projectId);
 
-    void deleteByProject_IdAndSoftware_Id(UUID projectId, UUID softwareId);
+    List<ProjectImage> findByProject_IdAndImageTypeOrderBySortOrderAsc(UUID projectId, ProjectImageType imageType);
 
 }
