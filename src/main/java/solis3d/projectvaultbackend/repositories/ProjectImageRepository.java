0da16f8f953e10fe5196efa42f400bfec3a17d28
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-05-18 17:44:28 +0200

    Update ProjectImageRepository.java

--- src/main/java/solis3d/projectvaultbackend/repositories/ProjectImageRepository.java
+++ src/main/java/solis3d/projectvaultbackend/repositories/ProjectImageRepository.java
@@ -3,14 +3,10 @@ package solis3d.projectvaultbackend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solis3d.projectvaultbackend.entities.ProjectImage;
 import solis3d.projectvaultbackend.entities.ProjectImageType;
-import solis3d.projectvaultbackend.entities.ProjectSoftware;
-
 import java.util.List;
 import java.util.UUID;
 
 public interface ProjectImageRepository extends JpaRepository<ProjectImage, UUID> {
-
-    List<ProjectSoftware> findByProject_Id(UUID projectId);
 
     List<ProjectImage> findByProject_IdOrderBySortOrderAsc(UUID projectId);
 
