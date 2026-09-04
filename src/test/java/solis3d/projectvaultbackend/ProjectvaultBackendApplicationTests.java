ee6116aa9faf151dbf8ec5ffbc8cfd30aa5d171d
Solis3D <127130916+Solis3D@users.noreply.github.com>
2026-09-04 12:43:51 +0200

    fix: isolate backend tests and make mail optional

--- src/test/java/solis3d/projectvaultbackend/ProjectvaultBackendApplicationTests.java
+++ src/test/java/solis3d/projectvaultbackend/ProjectvaultBackendApplicationTests.java
@@ -2,12 +2,13 @@ package solis3d.projectvaultbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectvaultBackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
